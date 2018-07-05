5136e5701137341c0b552881fe50b45e67d6f292
nvembar <navin.vembar@gmail.com>
2018-07-05 14:35:37 -0400

    Better variable management

--- create-sw-project.sh
+++ create-sw-project.sh
@@ -8,14 +8,28 @@ set -eu
 
 ORG_ID=
 PROJECT_NAME=
+PLAN_ONLY=1
 
 function usage {
-    echo "$0 <organization id> <project id> <project name>"
+    echo "$0 [-a] <organization id> <project id> <project name>"
+    echo ""
+    echo "     -a: Runs the application of the terraform script"
     echo ""
     echo "The project id must start with a letter and contain only alphanumerics "
     echo "and dashes."
     exit 1
 }
+
+while getopts "a" OPT; do
+    case $OPT in
+        a)
+            PLAN_ONLY=0
+            ;;
+        \?)
+            usage
+            ;;
+    esac
+done
 
 if [[ $# -ne 3 ]]
 then
@@ -50,8 +64,16 @@ project_id = "$PROJECT_ID"
 project_name = "$PROJECT_NAME"
 EOF
 
+PROJECT_DIR=projects/${PROJECT_ID}
+VAR_FILE=${PROJECT_DIR}/${PROJECT_ID}.tfvars
+PLAN_FILE=${PROJECT_DIR}/${PROJECT_ID}.plan.tf
+TFSTATE_FILE=${PROJECT_DIR}/terraform.tfstate
+
 terraform init -input=false projects/${PROJECT_ID}
 
-terraform plan -input=false -var-file=projects/${PROJECT_ID}/${PROJECT_ID}.tfvars \
-               -out=projects/${PROJECT_ID}/create-project.plan.tf \
-               projects/${PROJECT_ID}
+terraform plan -input=false -var-file=${VAR_FILE} -out=${PLAN_FILE} ${PROJECT_DIR}
+
+if [[ PLAN_ONLY -eq 0 ]]
+then
+    terraform apply -input=false -state=${TFSTATE_FILE} ${PLAN_FILE}
+fi
