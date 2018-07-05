1193b6c046c139d538d6a89c5994348ccf08d419
nvembar <navin.vembar@gmail.com>
2018-07-05 17:02:19 -0400

    Changed the way args are passed in

--- create-sw-project.sh
+++ create-sw-project.sh
@@ -8,22 +8,47 @@ set -eu
 
 ORG_ID=
 PROJECT_NAME=
+PROJECT_ID=
+DOMAIN=
+SUBDOMAIN=
 PLAN_ONLY=1
 
 function usage {
-    echo "$0 [-a] <organization id> <project id> <project name>"
+    echo "$0 -o <organization id> -p <project id> -d <domain> [-n <project name] [-s <subdomain>] [-a]"
     echo ""
-    echo "     -a: Runs the application of the terraform script"
+    echo "     -o <organization id> The GCP Organization ID [required]"
+    echo "     -p <project ID> The GCP Project ID to create in the Organization [required]"
+    echo "     -d <domain> The domain where the website will be created [required]"
+    echo "     -s <subdomain> The name of the subdomain to be built off the domain [optional]"
+    echo "     -n <project name> [optional]"
+    echo "     -a: Runs terraform apply to actually build the resources"
     echo ""
     echo "The project id must start with a letter and contain only alphanumerics "
     echo "and dashes."
+    echo ""
+    echo "If the subdomain or project name are not provided, the project ID will be used"
     exit 1
 }
 
-while getopts "a" OPT; do
+while getopts "an:d:s:o:p:" OPT; do
     case $OPT in
         a)
             PLAN_ONLY=0
+            ;;
+        n)
+            PROJECT_NAME=${OPTARG}
+            ;;
+        d)
+            DOMAIN=${OPTARG}
+            ;;
+        s)
+            SUBDOMAIN=${OPTARG}
+            ;;
+        o)
+            ORG_ID=${OPTARG}
+            ;;
+        p)
+            PROJECT_ID=${OPTARG}
             ;;
         \?)
             usage
@@ -31,24 +56,34 @@ while getopts "a" OPT; do
     esac
 done
 
-if [[ $(( $# - $OPTIND )) -ne 2 ]]
+# Check for the required arguments
+
+if [[ -z ${ORG_ID} || -z ${PROJECT_ID} || -z ${DOMAIN} ]]
 then
+    echo "One of the required arguments was not set"
+    echo ""
+    echo "ORG_ID=\'${ORG_ID}\' PROJECT_ID=\'${PROJECT_ID}\' DOMAIN=\'${DOMAIN}\'"
     usage
 fi
 
-ARGV=("$@")
+# Set from defaults
 
-ORG_ID=${ARGV[$((OPTIND - 1))]}
-PROJECT_ID=${ARGV[$((OPTIND))]}
-PROJECT_NAME=${ARGV[$((OPTIND + 1))]}
-
-if [[ -a ${PROJECT_ID} ]]
+if [[ -z ${SUBDOMAIN} ]]
 then
-    echo "A file associated with ${PROJECT_ID} already exists"
-    exit 1
+    SUBDOMAIN=${PROJECT_ID}
 fi
 
-# Need to test that this is a valid project name
+if [[ -z ${PROJECT_NAME} ]]
+then
+    PROJECT_NAME=${PROJECT_ID}
+fi
+
+PROJECT_DIR=projects/${PROJECT_ID}
+VAR_FILE=${PROJECT_DIR}/${PROJECT_ID}.tfvars
+PLAN_FILE=${PROJECT_DIR}/${PROJECT_ID}.plan.tf
+TFSTATE_FILE=${PROJECT_DIR}/terraform.tfstate
+
+# TODO: Need to test that this is a valid project name and if it already exists
 if [[ ! -a projects ]]
 then
     mkdir projects
@@ -58,22 +93,31 @@ then
     exit 1
 fi
 
+# Check if we already have a directory
+
+if [[ -a ${PROJECT_DIR} ]]
+then
+    echo "A file associated with ${PROJECT_ID} already exists"
+    exit 1
+fi
+
 cp -R create-project projects/${PROJECT_ID}
 
-cat << EOF > projects/${PROJECT_ID}/${PROJECT_ID}.tfvars
+cat << EOF > ${VAR_FILE}
 org_id = "$ORG_ID"
 project_id = "$PROJECT_ID"
 project_name = "$PROJECT_NAME"
+domain = "$DOMAIN"
+subdomain = "$SUBDOMAIN"
 EOF
 
-PROJECT_DIR=projects/${PROJECT_ID}
-VAR_FILE=${PROJECT_DIR}/${PROJECT_ID}.tfvars
-PLAN_FILE=${PROJECT_DIR}/${PROJECT_ID}.plan.tf
-TFSTATE_FILE=${PROJECT_DIR}/terraform.tfstate
+terraform init -input=false ${PROJECT_DIR}
 
-terraform init -input=false projects/${PROJECT_ID}
-
-terraform plan -input=false -var-file=${VAR_FILE} -out=${PLAN_FILE} ${PROJECT_DIR}
+terraform plan -input=false \
+               -var-file=${VAR_FILE} \
+               -out=${PLAN_FILE} \
+               -state=${PROJECT_DIR}/terraform.tfstate \
+               ${PROJECT_DIR}
 
 if [[ PLAN_ONLY -eq 0 ]]
 then
