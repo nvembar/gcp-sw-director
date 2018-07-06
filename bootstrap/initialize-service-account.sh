26c92276ee7d2ee1c6157fc4d0be6cc9ff2ebe3c
nvembar <navin.vembar@gmail.com>
2018-07-06 08:17:42 -0400

    Added reference to GCP/Terraform link

--- bootstrap/initialize-service-account.sh
+++ bootstrap/initialize-service-account.sh
@@ -3,4 +3,7 @@
 # Assumes that the user has been logged in on gcloud via gcloud auth login and 
 # has access to the organization
 
+# Most of this should be similar to what's found here:
+# https://cloud.google.com/community/tutorials/managing-gcp-projects-with-terraform
 
+# TODO: ...write this?
